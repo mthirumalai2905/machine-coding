7d4527291f1a8592757afdcb6dc5ecb7e41803bf
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2025-05-07 16:52:50 +0530

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+// Logger MiddleWare
+const logger = (req, res, next) => {
+    const currDate = new Date();
+    console.log(`${currDate} $[req.method] $[req.url]`);
+    next();
+};
+
+app.use(logger);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
